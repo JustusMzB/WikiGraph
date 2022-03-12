1f63010c9cb2e86b25a42b96b48cf85adb6df0c3
JustusMzB <ju@mzb.de>
2022-03-12 11:40:58 +0100

    Hopefully fixed strange Bug

--- reference_extractors/german_wiki_article_extractor.py
+++ reference_extractors/german_wiki_article_extractor.py
@@ -3,6 +3,9 @@ from turtle import onclick
 from reference_extractors.reference_extractor import ReferenceExtractor
 import re
 
+# Known Bug: For unknown reasons, sometimes Wikiarticles are created with urls from this source.
+# Said URLs, for unknown reasons, do not include a protocol extension such as ' https:// '
+# It is not reliably repeatable, but seems to happen at random.
 
 class GermanWikipediaArticleReferenceExtractor(ReferenceExtractor):
     """Extraktor für Artikel-Referenzen auf das deutsche Wikipedia 
@@ -20,13 +23,13 @@ class GermanWikipediaArticleReferenceExtractor(ReferenceExtractor):
         """
         # No hashtags, used in wikipedia for internal referencing
         wikipediarefs =  re.findall(r'<a\s+href="((?:(?:https?://)?de.wikipedia.org)?/wiki/(?:[^#"\s]|)+?)"[^<>]*', html)
-        # Fürs erste wird sich auf das deutsche Wikipedia beschränkt. Internationale Artikel könnten noch in neuer Klasse implementiert werden.
-        # Filtere hier für Wikipedia DE spezifische interne Seiten für Dateien, Kategorien etc. heraus.
-        only_article_refs: set[str] = set(filter(lambda ref: not re.match(r'/wiki/((Datei)|(Spezial)|(Kategorie)|(Wikipedia)|(Hilfe)|(Portal)):.*?', ref), wikipediarefs))
+
+        # These Keywords (In the german wikipedia space ) signal internal references to Kategorypages, Impressum etc.
+        only_article_refs = filter(lambda ref: not re.match(r'/wiki/((Datei)|(Spezial)|(Kategorie)|(Wikipedia)|(Hilfe)|(Portal)):.*?', ref), wikipediarefs)
+        # Using separate collections, as the use of one set was the most likely to be causing the strange Bug described above.
+        result_set = set()
         for ref in only_article_refs:
             # Turn all into absolute paths according to assumption of german wikipedia.
             if not ref.startswith("https://de.wikipedia.org"):
-                only_article_refs.add("https://de.wikipedia.org" + ref)
-                only_article_refs.remove(ref)
-
-        return only_article_refs
+                result_set.add("https://de.wikipedia.org" + ref)
+        return result_set
